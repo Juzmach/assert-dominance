1cb6dd94833d7a9c0288e6a6bc93a12e9bdcacc6
Juuso Strömberg <juzmach@w71.wlan.reaktor.fi>
2014-11-01 13:00:10 +0200

    Added task get and post

--- application.rb
+++ application.rb
@@ -1,11 +1,24 @@
 require 'sinatra'
-require 'models/Task'
+require 'data_mapper'
+require './models/Task'
 
 # Database connection
 DataMapper::setup(:default,"sqlite3://#{Dir.pwd}/database.db")
 
+DataMapper.auto_upgrade!
+
 get '/' do
+  @tasks = Task.all :order => :id.desc
   erb :index
+end
+
+post '/' do
+  task = Task.new
+  task.title = params[:title]
+  task.created_at = Time.now
+  task.updated_at = Time.now
+  task.save
+  redirect '/'
 end
 
 get "/kaljaa" do
--- models/task.rb
+++ models/task.rb
@@ -3,6 +3,7 @@ class Task
   property :id, Serial
   property :created_at, DateTime
   property :updated_at, DateTime
+  property :title, String
   property :status, String
   property :status_desc, Text
   property :priority, Integer
